49390bdde86b6b1516fee850689c889baf29fc7c
Saptashrungi <saptashrungib@gmail.com>
2021-02-26 16:04:09 +0530

    edited and added remove

--- xdaemon/cli/lookup.py
+++ xdaemon/cli/lookup.py
@@ -2,8 +2,11 @@ import json
 
 
 def store_job_by_id(**kwargs):  # id for lookup and job is string/filepath
+    with open('job_data.json', "r") as jsondata:
+        job_data = json.load(jsondata)
+    job_data.update(kwargs)
     with open("job_data.json", "w") as write_json:
-        json.dump(kwargs, write_json)
+        json.dump(job_data, write_json)
 
 
 def search_job_by_id(jobid):
@@ -12,10 +15,21 @@ def search_job_by_id(jobid):
 
     # load the json data
     if jobid in job_data:
-        return True
+        return job_data[jobid]
     else:
-        return False
+        return None
+
+def remove_job_by_id(jobid):
+    with open('job_data.json', "r") as jsondata:
+        job_data = json.load(jsondata)
+    if jobid in job_data:
+    	del job_data[jobid]
+    with open("job_data.json", "w") as write_json:
+        json.dump(job_data, write_json)
 
 store_job_by_id(jobid_1='xyz', jobid_2='abc')
 x = "jobid_2"
 print(search_job_by_id(x))
+remove_job_by_id("jobid_1")
+store_job_by_id(jobid_3='xyzv')
+
