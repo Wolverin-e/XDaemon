5ff4010eafcfd31dcc053a4e392f54f446e054ad
Saptashrungi <saptashrungib@gmail.com>
2021-02-26 16:08:48 +0530

    show added

--- xdaemon/cli/lookup.py
+++ xdaemon/cli/lookup.py
@@ -19,6 +19,7 @@ def search_job_by_id(jobid):
     else:
         return None
 
+
 def remove_job_by_id(jobid):
     with open('job_data.json', "r") as jsondata:
         job_data = json.load(jsondata)
@@ -27,9 +28,16 @@ def remove_job_by_id(jobid):
     with open("job_data.json", "w") as write_json:
         json.dump(job_data, write_json)
 
+
+def show_jobs():
+    with open('job_data.json', "r") as jsondata:
+        job_data = json.load(jsondata)
+    print(job_data)
+
 store_job_by_id(jobid_1='xyz', jobid_2='abc')
 x = "jobid_2"
 print(search_job_by_id(x))
 remove_job_by_id("jobid_1")
 store_job_by_id(jobid_3='xyzv')
+show_jobs()
 
