7c0a66111749d4bee9e694aa57be06b2ccd232a9
Mitul <patel.6@iitj.ac.in>
2021-03-08 20:54:53 +0530

    Added Basic colored logging setup.

--- xdaemon/cli/main.py
+++ xdaemon/cli/main.py
@@ -1,6 +1,8 @@
 import sys
 from docopt import docopt, DocoptExit
 from inspect import getdoc
+import logging
+import coloredlogs
 
 from .parser import YAMLJobParser
 from .executor import JobExecutor
@@ -13,6 +15,10 @@ from .lookup import (
     show_jobs,
     store_job_by_id,
     remove_job_by_id
+)
+from .formatting import (
+    level_styles,
+    field_styles
 )
 
 
@@ -31,6 +37,8 @@ def execute_command():
         'execute'
     ]
 
+    setup_logging()
+
     for command in commands:
         if opts[command]:
             getattr(Command, command)(opts)
@@ -46,6 +54,38 @@ def get_opts(argv):
         )
     except DocoptExit:
         raise SystemExit(doc_str)
+
+
+def setup_logging():
+    """
+    USAGE: in any file
+        from logging import getLogger
+        logger = getLogger(__name__)
+
+        logger.debug("Debug")          # 10
+        logger.info("Info")            # 20
+        logger.warn("Warn")            # 30
+        logger.warning("Warning")      # 30
+        logger.error("Error")          # 40
+        logger.fatal("Fatal")          # 50
+        logger.critical("Critical")    # 50
+    """
+    logging_format = (
+        '⟨{asctime} {msecs:.0f}ms⟩ [{levelno} {levelname:^.3}] › '
+        '{name} » {funcName}:{lineno:>3} → {message}'
+    )
+    date_format = '%d/%m/%y %I:%M:%S-%p'
+
+    coloredlogs.install(
+        level=logging.DEBUG,
+        fmt=logging_format,
+        datefmt=date_format,
+        style='{',
+        milliseconds=True,
+        level_styles=level_styles,
+        field_styles=field_styles,
+        stream=sys.stderr
+    )
 
 
 class Command:
